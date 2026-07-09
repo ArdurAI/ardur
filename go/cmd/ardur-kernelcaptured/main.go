ea8592a84fce44d0d348812024ef9b0cf1fb95eb
Harshit Jaiswal <you@example.com>
2026-07-09 23:11:26 +0530

    fix(ci): handle unchecked svr.Close() error in ardur-kernelcaptured daemon

--- go/cmd/ardur-kernelcaptured/main.go
+++ go/cmd/ardur-kernelcaptured/main.go
@@ -1289,7 +1289,9 @@ func main() {
 	<-ctx.Done()
 	log.Info("shutting down", "reason", ctx.Err())
 	_ = sdNotify("STOPPING=1")
-	svr.Close()
+	if err := svr.Close(); err != nil {
+		log.Error("failed to close control socket server", "error", err)
+	}
 	wg.Wait()
 	log.Info("ardur-kernelcaptured stopped")
 }
